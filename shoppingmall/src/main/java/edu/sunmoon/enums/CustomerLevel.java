d3a9eef104626c4a32195c035e61cd3f9b18386e
Jeong Yeon Kyun <jungyk411@gmail.com>
2024-09-27 17:28:20 +0900

    20240927

--- shoppingmall/src/main/java/edu/sunmoon/enums/CustomerLevel.java
+++
@@ -1,9 +0,0 @@
-package edu.sunmoon.enums;
-
-public enum CustomerLevel {
-    일반, 실버, 골드, VIP;
-
-    public static CustomerLevel fromString(String value) {
-        return valueOf(value);
-    }
-}
